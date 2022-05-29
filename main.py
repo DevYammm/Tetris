34f4eda0c666ed4f6819c326c587209f66fcae6a
DevYammm <dlsnatnlzl@naver.com>
2022-05-29 18:55:51 +0900

    4_block_moving - left right drop hard-drop rotate

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import down
 import pygame
 from random import *
 
@@ -41,7 +42,7 @@ def SpawnBlock(id):
     if id == 0:
         pass
     elif id == 1:
-        block_pos_0 = [5, 0]
+        block_pos_0 = [5, 2]
         block_pos_1 = [block_pos_0[0] - 1, block_pos_0[1]]
         block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
         block_pos_3 = [block_pos_0[0] + 2, block_pos_0[1]]
@@ -82,15 +83,15 @@ def SpawnBlock(id):
     game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
 
 # 블록 떨어지기
-def DropBlock():
-    global block_pos_0, block_pos_1, block_pos_2, block_pos_3, start_ticks, block_start_move, Running
-    drop_time = 2 / level
+def DroppingBlock():
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3, start_ticks, block_start_move, Running, rotate, drop_time
+    drop_time = 1.5 / level
     elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
     if elapsed_time > drop_time:
-        if (game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]]
-         or game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]]) == 8 \
-         or (game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]]
-         or game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]]) == 9:
+        if game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] == 8 or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]] == 8 or\
+           game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] == 8 or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]] == 8 or\
+           game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] == 8 or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]] == 9 or\
+           game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] == 8 or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]] == 9:
 
             # 블록이 바닥과 천장에 모두 닿았을 때 게임 오버
             if block_pos_0[1] == 0 or block_pos_1[1] == 0 or block_pos_2[1] == 0 or block_pos_3[1] == 0:
@@ -117,6 +118,204 @@ def DropBlock():
             game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
             game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
         
+def BlockMoveLeft():
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3
+    if game_grid_list[block_pos_0[1]][block_pos_0[0] - 1] == 8 or game_grid_list[block_pos_1[1]][block_pos_1[0] - 1] == 8 or\
+       game_grid_list[block_pos_2[1]][block_pos_2[0] - 1] == 8 or game_grid_list[block_pos_3[1]][block_pos_3[0] - 1] == 8 or\
+       game_grid_list[block_pos_0[1]][block_pos_0[0] - 1] == 9 or game_grid_list[block_pos_1[1]][block_pos_1[0] - 1] == 9 or\
+       game_grid_list[block_pos_2[1]][block_pos_2[0] - 1] == 9 or game_grid_list[block_pos_3[1]][block_pos_3[0] - 1] == 9:
+        print('wall!')
+    else:
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = 0
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = 0
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = 0
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = 0
+        block_pos_0 = [block_pos_0[0] - 1, block_pos_0[1]]
+        block_pos_1 = [block_pos_1[0] - 1, block_pos_1[1]]
+        block_pos_2 = [block_pos_2[0] - 1, block_pos_2[1]]
+        block_pos_3 = [block_pos_3[0] - 1, block_pos_3[1]]
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = id
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = id
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
+
+def BlockMoveRight():
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3
+    if game_grid_list[block_pos_0[1]][block_pos_0[0] + 1] == 8 or game_grid_list[block_pos_1[1]][block_pos_1[0] + 1] == 8 or\
+       game_grid_list[block_pos_2[1]][block_pos_2[0] + 1] == 8 or game_grid_list[block_pos_3[1]][block_pos_3[0] + 1] == 8 or\
+       game_grid_list[block_pos_0[1]][block_pos_0[0] + 1] == 9 or game_grid_list[block_pos_1[1]][block_pos_1[0] + 1] == 9 or\
+       game_grid_list[block_pos_2[1]][block_pos_2[0] + 1] == 9 or game_grid_list[block_pos_3[1]][block_pos_3[0] + 1] == 9:
+        print('wall!')
+    else:
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = 0
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = 0
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = 0
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = 0
+        block_pos_0 = [block_pos_0[0] + 1, block_pos_0[1]]
+        block_pos_1 = [block_pos_1[0] + 1, block_pos_1[1]]
+        block_pos_2 = [block_pos_2[0] + 1, block_pos_2[1]]
+        block_pos_3 = [block_pos_3[0] + 1, block_pos_3[1]]
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = id
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = id
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
+
+def Drop():
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3, block_start_move, drop_time
+    if game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] == 8 or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]] == 8 or\
+       game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] == 8 or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]] == 8 or\
+       game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] == 9 or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]] == 9 or\
+       game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] == 9 or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]] == 9:
+        elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
+        if elapsed_time > drop_time:
+            block_start_move = False
+
+    else:
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = 0
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = 0
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = 0
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = 0
+        block_pos_0 = [block_pos_0[0], block_pos_0[1] + 1]
+        block_pos_1 = [block_pos_1[0], block_pos_1[1] + 1]
+        block_pos_2 = [block_pos_2[0], block_pos_2[1] + 1]
+        block_pos_3 = [block_pos_3[0], block_pos_3[1] + 1]
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = id
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = id
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
+
+def HardDrop():
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3, block_start_move
+    game_grid_list[block_pos_0[1]][block_pos_0[0]] = 0
+    game_grid_list[block_pos_1[1]][block_pos_1[0]] = 0
+    game_grid_list[block_pos_2[1]][block_pos_2[0]] = 0
+    game_grid_list[block_pos_3[1]][block_pos_3[0]] = 0
+    while True:
+        if (game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]]
+         or game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]]) == 8 \
+         or (game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]]
+         or game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]]) == 9:
+
+            game_grid_list[block_pos_0[1]][block_pos_0[0]] = 9
+            game_grid_list[block_pos_1[1]][block_pos_1[0]] = 9
+            game_grid_list[block_pos_2[1]][block_pos_2[0]] = 9
+            game_grid_list[block_pos_3[1]][block_pos_3[0]] = 9
+            block_start_move = False
+            break
+        else:
+            block_pos_0 = [block_pos_0[0], block_pos_0[1] + 1]
+            block_pos_1 = [block_pos_1[0], block_pos_1[1] + 1]
+            block_pos_2 = [block_pos_2[0], block_pos_2[1] + 1]
+            block_pos_3 = [block_pos_3[0], block_pos_3[1] + 1]
+
+def RotateBlock(id, rot):
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3, rotate
+    if game_grid_list[block_pos_0[1] - 1][block_pos_0[0] - 1] == 8 or game_grid_list[block_pos_0[1]][block_pos_0[0] - 1] == 8 or\
+       game_grid_list[block_pos_0[1] + 1][block_pos_0[0] - 1] == 8 or game_grid_list[block_pos_0[1] - 1][block_pos_0[0]] == 8 or\
+       game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] == 8 or game_grid_list[block_pos_0[1] - 1][block_pos_0[0] + 1] == 8 or\
+       game_grid_list[block_pos_0[1]][block_pos_0[0] + 1] == 8 or game_grid_list[block_pos_0[1] + 1][block_pos_0[0] + 1] == 8 or\
+       game_grid_list[block_pos_0[1] - 1][block_pos_0[0] - 1] == 9 or game_grid_list[block_pos_0[1]][block_pos_0[0] - 1] == 9 or\
+       game_grid_list[block_pos_0[1] + 1][block_pos_0[0] - 1] == 9 or game_grid_list[block_pos_0[1] - 1][block_pos_0[0]] == 9 or\
+       game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] == 9 or game_grid_list[block_pos_0[1] - 1][block_pos_0[0] + 1] == 9 or\
+       game_grid_list[block_pos_0[1]][block_pos_0[0] + 1] == 9 or game_grid_list[block_pos_0[1] + 1][block_pos_0[0] + 1] == 9:
+            print("dhfb")
+            rotate -= 1
+            return
+    else:
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = 0
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = 0
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = 0
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = 0
+        if id == 1:
+            if rot == 0 or rot == 2:
+                rotate = 0
+                block_pos_1 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] + 2, block_pos_0[1]]
+            elif rot == 1:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_3 = [block_pos_0[0], block_pos_0[1] - 2]
+        elif id == 2:
+            if rot == 0 or rot == 4:
+                rotate = 0
+                block_pos_1 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0], block_pos_0[1] - 1]
+            elif rot == 1:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1]]
+            elif rot == 2:
+                block_pos_1 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0], block_pos_0[1] + 1]
+            elif rot == 3:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_2 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0], block_pos_0[1] - 1]
+        elif id == 3:
+            if rot == 0 or rot == 4:
+                rotate = 0
+                block_pos_1 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] - 1, block_pos_0[1] - 1]
+            elif rot == 1:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] - 1]
+            elif rot == 2:
+                block_pos_1 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] + 1]
+            elif rot == 3:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_3 = [block_pos_0[0] - 1, block_pos_0[1] + 1]
+        elif id == 4:
+            if rot == 0 or rot == 4:
+                rotate = 0
+                block_pos_1 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] - 1]
+            elif rot == 1:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] + 1]
+            elif rot == 2:
+                block_pos_1 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] - 1, block_pos_0[1] + 1]
+            elif rot == 3:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_3 = [block_pos_0[0] - 1, block_pos_0[1] - 1]
+        elif id == 6:
+            if rot == 0 or rot == 2:
+                rotate = 0
+                block_pos_1 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_3 = [block_pos_0[0] - 1, block_pos_0[1] - 1]
+            elif rot == 1:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] + 1]
+                block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] - 1]
+        elif id == 7:
+            if rot == 0 or rot == 2:
+                rotate = 0
+                block_pos_1 = [block_pos_0[0] - 1, block_pos_0[1]]
+                block_pos_2 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] - 1]
+            elif rot == 1:
+                block_pos_1 = [block_pos_0[0], block_pos_0[1] - 1]
+                block_pos_2 = [block_pos_0[0] + 1, block_pos_0[1]]
+                block_pos_3 = [block_pos_0[0] + 1, block_pos_0[1] + 1]
+
+        game_grid_list[block_pos_0[1]][block_pos_0[0]] = id
+        game_grid_list[block_pos_1[1]][block_pos_1[0]] = id
+        game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
+        game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
+            
 
 # 초기화
 pygame.init()
@@ -134,6 +333,7 @@ block_start_move = False
 drop_time = None
 start_ticks = None
 level = 1
+rotate = 0
 
 # color
 BLACK = (0, 0, 0)  # 0
@@ -152,6 +352,10 @@ block_pos_0 = []
 block_pos_1 = []
 block_pos_2 = []
 block_pos_3 = []
+
+k_left_speed = 0
+k_right_speed = 0
+k_down_speed = 0
 
 # 폰트
 game_font = pygame.font.Font(None, 120)
@@ -172,14 +376,44 @@ while Running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             Running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print('space')
+                HardDrop()
+            elif event.key == pygame.K_UP:
+                rotate += 1
+                RotateBlock(id, rotate)
 
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            k_left_speed += 0.1
+            if k_left_speed >= 5:
+                k_left_speed = 0
+                BlockMoveLeft()
+        elif event.key == pygame.K_RIGHT:
+            k_right_speed += 0.1
+            if k_right_speed >= 5:
+                k_right_speed = 0
+                BlockMoveRight()
+        elif event.key == pygame.K_DOWN:
+            k_down_speed += 0.1
+            if k_down_speed >= 5 // level:
+                k_down_speed = 0
+                Drop()
+
+    elif event.type == pygame.KEYUP:
+        k_left_speed = 0
+        k_right_speed = 0
+        k_down_speed = 0
+        
     if block_start_move == False:
+        rotate = 0
         id = randrange(1, 8)
         start_ticks = pygame.time.get_ticks()
         SpawnBlock(id)
         block_start_move = True
 
-    DropBlock()
+    DroppingBlock()
     screen.fill(BLACK)
     BlockPrint()
 
@@ -192,5 +426,5 @@ while Running:
 # screen.blit(msg,msg_rect)
 
 # 3초 보여주고 종료
-pygame.time.delay(3000)
+#pygame.time.delay(3000)
 pygame.quit()
