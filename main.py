5f56e61fa3fc92fa673cf17a106451b0b6e627a2
DevYammm <dlsnatnlzl@naver.com>
2022-05-29 13:20:25 +0900

    3_block drop and game over

--- main.py
+++ main.py
@@ -1,51 +1,6 @@
 import pygame
 from random import *
 
-# 초기화
-pygame.init()
-screen_width = 300
-screen_height = 500
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Tetris")
-
-# 기본 세팅 값
-rows = 20
-columns = 12
-cell_size = 25
-block_size = 21
-cell_margin = 2
-
-# color
-BLACK = (0, 0, 0)  # 0
-MINT = (0, 255, 255)  # 1
-PURPLE = (127, 0, 255)  # 2
-ORANGE = (255, 128, 0)  # 3
-BLUE = (0, 0, 255)  # 4
-YELLOW = (255, 255, 0)  # 5
-GREEN = (0, 255, 0)  # 6
-RED = (255, 0, 0)  # 7
-WHITE = (255, 255, 255)  # 8
-
-
-block_start_move = False
-
-# 기본 틀 그리기
-game_grid_list = [[0 for col in range(columns)] for row in range(rows)]
-for idx, grid in enumerate(game_grid_list):
-    game_grid_list[idx][0] = 8
-    game_grid_list[idx][columns - 1] = 8
-for idx, grid in enumerate(game_grid_list[rows - 1]):
-    game_grid_list[rows - 1][idx] = 8
-
-def DrawBlock(col_idx, row_idx, color):
-    center_x = (col_idx * cell_size) + (cell_size / 2)
-    center_y = (row_idx * cell_size) + (cell_size / 2)
-
-    block = pygame.Rect(0, 0, block_size, block_size)
-    block.center = (center_x, center_y)
-
-    pygame.draw.rect(screen, color, block)
-
 # 게임 스크린 그리기
 def BlockPrint():
     for row_idx, row in enumerate(game_grid_list):
@@ -66,8 +21,22 @@ def BlockPrint():
                 DrawBlock(col_idx, row_idx, GREEN)
             elif col == 7:
                 DrawBlock(col_idx, row_idx, RED)
-                
+            elif col == 9:
+                DrawBlock(col_idx, row_idx, GRAY)
+
+# 블록 그리기
+def DrawBlock(col_idx, row_idx, color):
+    center_x = (col_idx * cell_size) + (cell_size / 2)
+    center_y = (row_idx * cell_size) + (cell_size / 2)
+
+    block = pygame.Rect(0, 0, block_size, block_size)
+    block.center = (center_x, center_y)
+
+    pygame.draw.rect(screen, color, block)
+
+# 블록 스폰하기
 def SpawnBlock(id):
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3
     # 인덱스 0은 x, 1은 y좌표를 의미
     if id == 0:
         pass
@@ -110,7 +79,91 @@ def SpawnBlock(id):
     game_grid_list[block_pos_0[1]][block_pos_0[0]] = id
     game_grid_list[block_pos_1[1]][block_pos_1[0]] = id
     game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
-    game_grid_list[block_pos_3[1]][block_pos_3[0]] = id    
+    game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
+
+# 블록 떨어지기
+def DropBlock():
+    global block_pos_0, block_pos_1, block_pos_2, block_pos_3, start_ticks, block_start_move, Running
+    drop_time = 2 / level
+    elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
+    if elapsed_time > drop_time:
+        if (game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]]
+         or game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]]) == 8 \
+         or (game_grid_list[block_pos_0[1] + 1][block_pos_0[0]] or game_grid_list[block_pos_1[1] + 1][block_pos_1[0]]
+         or game_grid_list[block_pos_2[1] + 1][block_pos_2[0]] or game_grid_list[block_pos_3[1] + 1][block_pos_3[0]]) == 9:
+
+            # 블록이 바닥과 천장에 모두 닿았을 때 게임 오버
+            if block_pos_0[1] == 0 or block_pos_1[1] == 0 or block_pos_2[1] == 0 or block_pos_3[1] == 0:
+                Running = False  
+                return
+            
+            block_start_move = False
+            game_grid_list[block_pos_0[1]][block_pos_0[0]] = 9
+            game_grid_list[block_pos_1[1]][block_pos_1[0]] = 9
+            game_grid_list[block_pos_2[1]][block_pos_2[0]] = 9
+            game_grid_list[block_pos_3[1]][block_pos_3[0]] = 9
+        else:
+            start_ticks = pygame.time.get_ticks()
+            game_grid_list[block_pos_0[1]][block_pos_0[0]] = 0
+            game_grid_list[block_pos_1[1]][block_pos_1[0]] = 0
+            game_grid_list[block_pos_2[1]][block_pos_2[0]] = 0
+            game_grid_list[block_pos_3[1]][block_pos_3[0]] = 0
+            block_pos_0 = [block_pos_0[0], block_pos_0[1] + 1]
+            block_pos_1 = [block_pos_1[0], block_pos_1[1] + 1]
+            block_pos_2 = [block_pos_2[0], block_pos_2[1] + 1]
+            block_pos_3 = [block_pos_3[0], block_pos_3[1] + 1]
+            game_grid_list[block_pos_0[1]][block_pos_0[0]] = id
+            game_grid_list[block_pos_1[1]][block_pos_1[0]] = id
+            game_grid_list[block_pos_2[1]][block_pos_2[0]] = id
+            game_grid_list[block_pos_3[1]][block_pos_3[0]] = id
+        
+
+# 초기화
+pygame.init()
+screen_width = 300
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Tetris")
+
+# 기본 세팅 값
+rows = 20
+columns = 12
+cell_size = 25
+block_size = 21
+block_start_move = False
+drop_time = None
+start_ticks = None
+level = 1
+
+# color
+BLACK = (0, 0, 0)  # 0
+MINT = (0, 255, 255)  # 1
+PURPLE = (127, 0, 255)  # 2
+ORANGE = (255, 128, 0)  # 3
+BLUE = (0, 0, 255)  # 4
+YELLOW = (255, 255, 0)  # 5
+GREEN = (0, 255, 0)  # 6
+RED = (255, 0, 0)  # 7
+WHITE = (255, 255, 255)  # 8
+GRAY = (128, 128, 128)  # 9
+
+# 현재 블록의 위치
+block_pos_0 = []
+block_pos_1 = []
+block_pos_2 = []
+block_pos_3 = []
+
+# 폰트
+game_font = pygame.font.Font(None, 120)
+
+# 기본 틀 그리기
+game_grid_list = [[0 for col in range(columns)] for row in range(rows)]
+for idx, grid in enumerate(game_grid_list):
+    game_grid_list[idx][0] = 8
+    game_grid_list[idx][columns - 1] = 8
+for idx, grid in enumerate(game_grid_list[rows - 1]):
+    game_grid_list[rows - 1][idx] = 8
+
 
 # main
 Running = True
@@ -122,11 +175,22 @@ while Running:
 
     if block_start_move == False:
         id = randrange(1, 8)
+        start_ticks = pygame.time.get_ticks()
         SpawnBlock(id)
         block_start_move = True
+
+    DropBlock()
     screen.fill(BLACK)
     BlockPrint()
 
     pygame.display.update()  # !!! 제발 마지막에 작성 꼭! 해주기 !!!
 
+
+# msg = game_font.render(f"Your level is {level}", True, WHITE)
+# msg_rect = msg.get_rect(center=(screen_width/2, screen_height/2))
+# screen.fill(BLACK)
+# screen.blit(msg,msg_rect)
+
+# 3초 보여주고 종료
+pygame.time.delay(3000)
 pygame.quit()
